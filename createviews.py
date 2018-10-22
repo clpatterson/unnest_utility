120d2b020b5e9525245a7fc0a0f909b954fe1dbb
System Administrator <root@Charless-MBP.fios-router.home>
2018-10-21 20:27:53 -0400

    Revise parse_table_schema output as dict list

--- createviews.py
+++ createviews.py
@@ -7,10 +7,18 @@ to flatten the table, executes the query in BQ, then saves the view.
 
 """
 
-import re
+import os,re
 from google.cloud import bigquery
 from google.cloud.bigquery.schema import SchemaField
 
+
+# TODO: return file path of 'service_account.json'file on user system.
+# HINT: use os.walk to search from User level down or Desktop down. 
+
+# Set credentials
+# NOTE: must be changed by user to reflect correct file path.
+os.environ['GOOGLE_APPLICATION_CREDENTIALS']="/Users/Work/Desktop/\
+scripting/gcp/bq/service_account/service_account.json"
 
 # Connect to a project.
 project_name = 'cool-academy-217719'
@@ -26,6 +34,9 @@ table_ref = dataset_ref.table(table_name)
 table = client.get_table(table_ref)
 table_schema = table.schema
 
+# Define primary key
+primary_key = '_id'
+
 def unpack_table_schema(table_schema):
 	"""Convert table schema to list of python dicts."""
 	fields = []
@@ -37,7 +48,7 @@ def unpack_table_schema(table_schema):
 fields = unpack_table_schema(table_schema)
 
 # Algorithm parses table schema.
-def parse_table_schema(fields, table='', select_statement=[]):
+def parse_table_schema(fields, table='', views=[]):
 	"""Parse schema, track lineage, and return list of fields."""
 	
 	for field in fields:
@@ -45,117 +56,184 @@ def parse_table_schema(fields, table='', select_statement=[]):
 		field_mode = field['mode']
 		field_name_unicode = field['name']
 		field_name = field_name_unicode.encode('ascii')
+		# Take out primary key
+		if field_name is primary_key:
+			pass
 		# Add record fields.
 		if field_mode != u'REPEATED' and field_type == u'RECORD':
-			parent = field_name
-			for field in field['fields']:
-				field_name_unicode = field['name']
-				field_name = field_name_unicode.encode('ascii')
-				# TODO: Flag these fields appropriately
-				select_statement.append(parent + '.' + field_name + '_RECORD')
+			if not table == '':
+				table = table + '.' + field_name
+				fields = field['fields']
+				# Add records flag
+				for field in fields:
+					field['record'] = table
+				views += parse_table_schema(fields, table, [])
+				table = ''
+			else:
+				table = field_name
+				fields = field['fields']
+				# Add records flag
+				for field in fields:
+					field['record'] = table
+				views += parse_table_schema(fields, table, [])
+				table = ''
 		# Add non-repeated fields.
 		elif field_mode != u'REPEATED':
-			select_statement.append(table + '.' + field_name)
+			table = table
+			field_name = table + '.' + field_name
+			# Check to if record key exits
+			try:
+				record = field['record']
+			except KeyError:
+				views.append({
+					'view_names':table, 
+					'field_name':field_name
+					})
+			else:
+				views.append({
+					'view_names':table, 
+					'field_name':field_name, 
+					'record': record
+					})
 		else:
 			# Add repeated non-record fields 
 			if field['type'] != u'RECORD':
-				field_type = field_type.encode('ascii')
-				# TODO: Flag these fields appropriately
-				select_statement.append(table + '.' + field_name + 'R_' + field_type)
+				table = table
+				field_name = table + '.' + field_name
+				method = 'ARRAY_LENGTH({})'.format(field_name)
+				# Check to if record key exits
+				try:
+					record = field['record']
+				except KeyError:
+					views.append({
+						'view_names':table, 
+						'field_name':field_name, 
+						'method':method
+						})
+				else:
+					views.append({'view_names':table, 
+						'field_name':field_name, 
+						'method':method, 
+						'record': record})
 			# Unpack repeated record fields
 			else:
-				table = table + '.' + field_name
-				fields = field['fields']
-				select_statement += parse_table_schema(fields, table, [])
-				table = ''
-	return select_statement
+				if not table == '':
+					table = table + '.' + field_name
+					fields = field['fields']
+					views += parse_table_schema(fields, table, [])
+					table = ''
+				else:
+					table = field_name
+					fields = field['fields']
+					views += parse_table_schema(fields, table, [])
+					table = ''
+
+	return views
 
 fields = parse_table_schema(fields)
+for field in fields:
+	print(field)
 
-# TOD0: Test parse_table_schema function with triple nested schema.	
-def clean_fields(fields):
-	"""Remove '.' from beginning of field names."""
-	clean_fields = []
-	for field in fields:
-		if field[0] is '.':
-			clean_fields.append(field[1:])
-		else:
-			clean_fields.append(field)
-	return clean_fields
+## TOD0: Test parse_table_schema function with triple nested schema.	
+#def clean_fields(fields):
+#	"""Remove '.' from beginning of field names."""
+#	clean_fields = []
+#	for field in fields:
+#		if field[0] is '.':
+#			clean_fields.append(field[1:])
+#		else:
+#			clean_fields.append(field)
+#	return clean_fields
+#
+#fields = clean_fields(fields)
+##for field in fields:
+##	print(field)
+#
+## TODO: Determine which views to create.
+#def views_planning(fields):
+#	view_name = []
+#	views = []
+#	for field in fields:
+#		if not '.' in field:
+#			view_name.append(table_name)
+#			views.append({'view':table_name,'field':field})
+#
+#	return views
+#
+#print(views_planning(fields))
+#
 
-fields = clean_fields(fields)
-
-def write_select_clause(fields):
-	"""Format fields for select clause of SQL query."""
-	select_string = ''
-	for field in fields:
-		if '_RECORD' in field:
-			field = field[:-7]
-		if 'R_' in field:
-			field = field.split('R')
-			field = field[0]
-		if '.' in field:
-			alias = re.sub(r'\.', '_', field)
-			select_string = select_string + field + ' AS ' + alias + ', ' 
-		else:
-			select_string = select_string + field + ', '
-	select_string = select_string[:-2]
-	
-	return select_string
-
-select = write_select_clause(fields)
-
-
-def write_table_path(project_name, dataset_name, table_name):
-	bq_table_path = '{0}.{1}.{2}'.format(project_name,
-		dataset_name,
-		table_name)
-	return bq_table_path
-
-bq_table_path = write_table_path(project_name, dataset_name, table_name)
-
-def write_from_clause(bq_table_path, fields):
-	# Cycle through fields and find columns to unnest
-	unnest = []
-	for field in fields:
-		if '_RECORD' in field:
-			pass
-		elif '.' in field:
-			index = [pos for pos, char in enumerate(field) if char == '.']
-			for value in index:
-				need_unnest = field[:value]
-				need_unnest = 'UNNEST({})'.format(need_unnest)
-				unnest.append(need_unnest)
-	
-	unnest = sorted(set(unnest))
-	unnest_string = ''
-	for field in unnest:
-		unnest_string = unnest_string + field + ', '
-	unnest_string = unnest_string[:-2]
-	
-	return unnest_string
-
-unnest_clause = write_from_clause(bq_table_path, fields)
-
-
-# TODO: Algorithm to write SQL query.
-def assemble_top_level_query(select, bq_table_path, unnest_clause):
-	"""Write SQL query from clauses."""
-	if len(unnest_clause) is not 0:
-		comma = ','
-	else:
-		comma = ''
-
-	query = 'SELECT {0} FROM `{1}`{2}{3};'.format(
-		select,
-		bq_table_path,
-		comma,
-		unnest_clause)
-	
-	return query
-
-query = assemble_top_level_query(select,bq_table_path,unnest_clause)
-print(query)
+#def write_select_clause(fields):
+#	"""Format fields for select clause of SQL query."""
+#	select_string = ''
+#	for field in fields:
+#		if '_RECORD' in field:
+#			field = field[:-7]
+#		if 'R_' in field:
+#			field = field.split('R')
+#			field = field[0]
+#		if '.' in field:
+#			alias = re.sub(r'\.', '_', field)
+#			select_string = select_string + field + ' AS ' + alias + ', ' 
+#		else:
+#			select_string = select_string + field + ', '
+#	select_string = select_string[:-2]
+#	
+#	return select_string
+#
+#select = write_select_clause(fields)
+#
+#
+#def write_table_path(project_name, dataset_name, table_name):
+#	bq_table_path = '{0}.{1}.{2}'.format(project_name,
+#		dataset_name,
+#		table_name)
+#	return bq_table_path
+#
+#bq_table_path = write_table_path(project_name, dataset_name, table_name)
+#
+#def write_from_clause(bq_table_path, fields):
+#	# Cycle through fields and find columns to unnest
+#	unnest = []
+#	for field in fields:
+#		if '_RECORD' in field:
+#			pass
+#		elif '.' in field:
+#			index = [pos for pos, char in enumerate(field) if char == '.']
+#			for value in index:
+#				need_unnest = field[:value]
+#				need_unnest = 'UNNEST({})'.format(need_unnest)
+#				unnest.append(need_unnest)
+#	
+#	unnest = sorted(set(unnest))
+#	unnest_string = ''
+#	for field in unnest:
+#		unnest_string = unnest_string + field + ', '
+#	unnest_string = unnest_string[:-2]
+#	
+#	return unnest_string
+#
+#unnest_clause = write_from_clause(bq_table_path, fields)
+#
+#
+## TODO: Algorithm to write SQL query.
+#def assemble_top_level_query(select, bq_table_path, unnest_clause):
+#	"""Write SQL query from clauses."""
+#	if len(unnest_clause) is not 0:
+#		comma = ','
+#	else:
+#		comma = ''
+#
+#	query = 'SELECT {0} FROM `{1}`{2}{3};'.format(
+#		select,
+#		bq_table_path,
+#		comma,
+#		unnest_clause)
+#	
+#	return query
+#
+#query = assemble_top_level_query(select,bq_table_path,unnest_clause)
+#print(query)
 
 # TODO: Add commas to select statements (last item has no comma after)
 # TODO: Add UNNEST() to unnest statements
